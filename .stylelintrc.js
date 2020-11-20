27da449e56f41ad3be93afaf92b172bf738cd33c
dman54 <dr.d-man54@yandex.ru>
2020-11-20 14:54:06 +0700

    stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["stylelint-config-airbnb",
+            "stylelint-prettier/recommended",
+            "stylelint-config-rational-order"],
+  plugins: ["stylelint-order", "stylelint-scss"],
+  rules: {
+  }
+};
